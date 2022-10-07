ee52f524f0005909198791004277e6364acb8bfe
Egor Ivanov <eai84199@gmail.com>
2022-10-07 22:14:47 +0700

    lines work, need to fix bugs

--- WeightControlInterface/WeightControlInterface/app2_customblock.h
+++ WeightControlInterface/WeightControlInterface/app2_customblock.h
@@ -8,14 +8,57 @@
 #include <QDebug>
 #include <QCursor>
 #include <QMenu>
+#include <QGraphicsLineItem>
 #include <QGraphicsSceneEvent>
 #include "app2_resizenode.h"
+
+#include <iostream>
 
 class APP2_simpleblock;
 class APP2_constantblock;
 class APP2_resizenode;
 class APP2_signalnode;
 class APP2_slotnode;
+class APP2_connectline;
+
+static APP2_connectline * connect_line_buf = nullptr;
+
+/* @brief
+ * Connection Line object
+ */
+
+class APP2_connectline : public QObject, public QGraphicsLineItem {
+    Q_OBJECT
+
+public:
+
+    void SetStart(QPointF new_dot){
+        this->setPos(new_dot + QPointF(10, 5));
+        this->setLine(QLineF(new_dot, this->line().p2()));
+    }
+    void SetEnd(QPointF new_dot){
+        this->setLine(QLineF(this->line().p1(), new_dot));
+    }
+
+    APP2_connectline(QPointF start, QPointF end){
+        this->setPos(start.x() + 10, start.y() + 5);
+        this->setLine(QLineF(start, end));
+    }
+
+    QRectF boundingRect() const override {
+        return QRectF(0, 0, this->line().dx(), this->line().dy());
+    }
+    void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget) override{
+        painter->setPen(QPen(Qt::black, 2));
+        painter->drawLine(0, 0, this->line().dx() / 2, 0);
+        painter->drawLine(this->line().dx() / 2, 0, this->line().dx() / 2, this->line().dy());
+        painter->drawLine(this->line().dx() / 2, this->line().dy(), this->line().dx(), this->line().dy());
+        this->scene()->update();
+    }
+
+    APP2_signalnode *   sig = nullptr;
+    APP2_slotnode *     slot = nullptr;
+};
 
 /* @brief
  * Slot node for the object
@@ -28,12 +71,17 @@ private:
     QRectF size;
 
 public:
+
+    APP2_connectline * connect_line = nullptr;
+
     APP2_slotnode(void){
+        std::cout << this << std::endl;
+        this->setToolTip("Slot");
         this->setCursor(QCursor(Qt::CrossCursor));
         this->size.setX(0);
         this->size.setY(0);
-        this->size.setHeight(10);
-        this->size.setWidth(10);
+        this->size.setHeight(30);
+        this->size.setWidth(30);
     }
     QRectF boundingRect() const override{
         return QRectF(this->size);
@@ -44,7 +92,7 @@ public:
         tmp_br.setColor(QColor(175, 0, 0));
         tmp_br.setStyle(Qt::SolidPattern);
         painter->setBrush(tmp_br);
-        QPointF pts[5] = { QPointF(0, 0), QPointF(10, 0), QPointF(10, 10), QPointF(0, 10), QPointF(5, 5) };
+        QPointF pts[5] = { QPointF(10, 10), QPointF(20, 10), QPointF(20, 20), QPointF(10, 20), QPointF(15, 15) };
         painter->drawPolygon(pts, 5);
     }
 
@@ -52,18 +100,35 @@ public:
 
 protected:
     void mousePressEvent(QGraphicsSceneMouseEvent * event){
+        if (event->button() == Qt::LeftButton){
 
+        }
     }
     void mouseReleaseEvent(QGraphicsSceneMouseEvent * event){
+        if (event->button() == Qt::LeftButton){
 
+        }
     }
     void mouseMoveEvent(QGraphicsSceneMouseEvent * event){
+        if (this->flClicked){
 
+        }
+        else{
+//            if (connect_line_buf != nullptr){
+//                connect_line_buf->slot = this;
+//                connect_line_buf->SetEnd(QPointF(this->pos().x() + 5, this->pos().y() + 5));
+//                qDebug() << "Slot mouse move event";
+//            }
+        }
     }
 
 public slots:
     void slMoveTo(QPointF new_pos){
         this->setPos(new_pos);
+//        qDebug() << "Move line " << (this->connect_line == nullptr);
+        if (this->connect_line != nullptr){
+            this->connect_line->SetEnd(new_pos);
+        }
     }
 
 signals:
@@ -82,12 +147,15 @@ private:
     QRectF size;
 
 public:
+    APP2_connectline * signal_line = nullptr;
+
     APP2_signalnode(void){
+        this->setToolTip("Signal");
         this->setCursor(QCursor(Qt::CrossCursor));
         this->size.setX(0);
         this->size.setY(0);
-        this->size.setHeight(10);
-        this->size.setWidth(10);
+        this->size.setHeight(30);
+        this->size.setWidth(30);
     }
     QRectF boundingRect() const override{
         return QRectF(this->size);
@@ -98,7 +166,7 @@ public:
         tmp_br.setColor(QColor(0, 175, 0));
         tmp_br.setStyle(Qt::SolidPattern);
         painter->setBrush(tmp_br);
-        QPointF pts[5] = { QPointF(0, 0), QPointF(5, 0), QPointF(10, 5), QPointF(5, 10), QPointF(0, 10) };
+        QPointF pts[5] = { QPointF(10, 10), QPointF(15, 10), QPointF(20, 15), QPointF(15, 20), QPointF(10, 20) };
         painter->drawPolygon(pts, 5);
     }
 
@@ -106,18 +174,55 @@ public:
 
 protected:
     void mousePressEvent(QGraphicsSceneMouseEvent * event){
-
+        if (event->button() == Qt::LeftButton){
+            this->flClicked = true;
+            this->signal_line = new APP2_connectline(this->pos() + QPointF(10, 10), this->pos());
+            this->signal_line->sig = this;
+            this->scene()->addItem(this->signal_line);
+        }
     }
     void mouseReleaseEvent(QGraphicsSceneMouseEvent * event){
-
+        if (event->button() == Qt::LeftButton){
+            this->flClicked = false;
+            if (this->signal_line->slot == nullptr){
+                this->scene()->removeItem(this->signal_line);
+                this->signal_line = nullptr;
+            }
+            else{
+                this->signal_line->slot->connect_line = this->signal_line;
+            }
+        }
     }
     void mouseMoveEvent(QGraphicsSceneMouseEvent * event){
-
+        if (this->flClicked){
+            QGraphicsItem * item;
+            if ((item = this->scene()->itemAt(event->scenePos(), QTransform())) != nullptr){
+                if (item->toolTip() == "Slot"){
+                    this->signal_line->SetEnd(item->pos() + QPointF(5, 10));
+                    int * tmp = reinterpret_cast<int *>(item);
+                    tmp -= 4;
+                    this->signal_line->slot = reinterpret_cast<APP2_slotnode *>(tmp);
+                    std::cout << "Reinterpret Item: " << this->signal_line->slot << std::endl;
+                    std::cout << "Basic Item: " << item << std::endl;
+                }
+                else{
+                    this->signal_line->SetEnd(event->scenePos());
+                    this->signal_line->slot = nullptr;
+                }
+            }
+            else{
+                this->signal_line->SetEnd(event->scenePos());
+                this->signal_line->slot = nullptr;
+            }
+        }
     }
 
 public slots:
     void slMoveTo(QPointF new_pos){
         this->setPos(new_pos);
+        if (this->signal_line != nullptr){
+            this->signal_line->SetStart(new_pos + QPointF(10, 10));
+        }
     }
 
 signals:
@@ -142,11 +247,11 @@ private:
         this->type->setPos(this->pos() + QPointF(0, -20));
         uint16_t it = 0;
         for (auto iter = this->signalnodes.begin(); iter != this->signalnodes.end(); iter++){
-            (*iter)->slMoveTo(this->pos() + QPointF(this->size.width() - 3, this->size.height() / (this->signalnodes.count() + 1) * (it++ + 1)));
+            (*iter)->slMoveTo(this->pos() + QPointF(this->size.width() - 13, this->size.height() / (this->signalnodes.count() + 1) * (it++ + 1) - 10));
         }
         it = 0;
         for (auto iter = this->slotnodes.begin(); iter != this->slotnodes.end(); iter++){
-            (*iter)->slMoveTo(this->pos() + QPointF(-7, this->size.height() / (this->slotnodes.count() + 1) * (it++ + 1)));
+            (*iter)->slMoveTo(this->pos() + QPointF(-17, this->size.height() / (this->slotnodes.count() + 1) * (it++ + 1) - 10));
         }
     }
 
